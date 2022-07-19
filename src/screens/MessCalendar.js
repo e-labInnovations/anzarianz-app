cd32cad3988005e3749d511c1efa9b7244998f4f
e-labinnovations <mohammedashad23@gmail.com>
2022-07-19 23:56:20 +0530

    leave add modal started

--- src/screens/MessCalendar.js
+++ src/screens/MessCalendar.js
@@ -1,18 +1,39 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { SafeAreaView, StyleSheet, View, Text, StatusBar } from 'react-native'
-import {Calendar} from 'react-native-calendars';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  Modal,
+  TouchableOpacity,
+  TextInput
+} from 'react-native'
+import { Calendar } from 'react-native-calendars';
 import axios from 'axios'
 import { AuthContext } from "../context/AuthContext";
 import { BASE_URL } from '../config';
-import Toast from 'react-native-toast-message';
 import { FloatingAction } from "react-native-floating-action";
 import moment from 'moment';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const MessCalendar = () => {
   const { userToken } = useContext(AuthContext)
+  const { userInfo } = useContext(AuthContext)
   const [markedDates, setMarkedDates] = useState({})
   const [leaves, setLeaves] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const actions = [
+    {
+      text: "Add New Leave",
+      icon: <Ionicons name="add" size={24} color="#fff" />,
+      name: "bt_add_new_leave",
+      position: 1
+    }
+  ];
 
   const handleDayPress = (day) => {
     console.log(day)
@@ -79,14 +100,48 @@ const MessCalendar = () => {
       />
       <Text>{JSON.stringify(leaves)}</Text>
       <FloatingAction
-        onPressMain={() => {
-          Toast.show({
-            type: 'success',
-            text1: 'Hello',
-            text2: 'This is some something 👋'
-          });
+        actions={actions}
+        onPressItem={name => {
+          if(name == 'bt_add_new_leave') {
+            setModalVisible(true)
+          }
         }}
+        // visible={!modalVisible} 
       />
+
+      <View style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+
+              <Text style={styles.mainText}>Add New Leave</Text>
+              <View style={styles.inputView}>
+                <FontAwesome5 name="user-circle" size={20} color="#666" style={styles.inputIcon} />
+                <TextInput placeholder='Name' style={styles.input} value={userInfo.name} editable={false} />
+              </View>
+              <View style={styles.inputView}>
+                <FontAwesome5 name="user-circle" size={20} color="#666" style={styles.inputIcon} />
+                <TextInput placeholder='Room NO' style={styles.input} value={userInfo.room_no} editable={false} />
+              </View>
+              <View style={styles.inputView}>
+                <Ionicons name="ios-lock-closed-outline" size={20} color="#666" style={styles.inputIcon} />
+                <TextInput placeholder='Username' style={styles.input} />
+              </View>
+              
+              <TouchableOpacity onPress={() => {setModalVisible(!modalVisible)}} style={styles.button}>
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </View>
+
     </SafeAreaView>
     
   )
@@ -94,9 +149,46 @@ const MessCalendar = () => {
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      marginTop:StatusBar.currentHeight,
-      backgroundColor: '#fff'
+    flex: 1,
+    marginTop:StatusBar.currentHeight,
+    backgroundColor: '#fff',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    margin: 25
+  },
+  mainText:{
+    fontFamily: 'roboto-medium',
+    fontSize: 28,
+    fontWeight: '500',
+    color: '#333',
+    marginBottom: 30,
+  },
+  inputView: {
+    flexDirection: 'row',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    paddingBottom: 8,
+    marginBottom: 25
+  },
+  inputIcon: {
+    marginRight: 5
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#AD40AF',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 30
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#FFF'
   }
 });
 
