9eb2db7316f4ae93750d692532240b5fda4c51cd
e-labinnovations <mohammedashad23@gmail.com>
2022-07-30 13:36:44 +0530

    Save pushToken to server

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default function App() {
   const [notification, setNotification] = useState(false);
   const notificationListener = useRef();
   const responseListener = useRef();
-
+  
   const [fontsLoaded] = useFonts({
     'roboto-medium-italic' : require('./assets/fonts/Roboto-MediumItalic.ttf'),
     'roboto-medium' : require('./assets/fonts/Roboto-Medium.ttf'),
@@ -51,7 +51,37 @@ export default function App() {
     };
   }, [])
   
-  
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+      updatePushToken(token)
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
+    return token;
+  }
 
   if (!fontsLoaded) {
     return null //<AppLoading/>
@@ -63,36 +93,4 @@ export default function App() {
       <Toast />
     </AuthProvider>
   )
-}
-
-async function registerForPushNotificationsAsync() {
-  let token;
-  if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
-    // updatePushToken(token)
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  return token;
 }
--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -1,30 +1,32 @@
-import react, { useContext } from "react";
+import react from "react";
 import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { BASE_URL } from "../config";
-import { AuthContext } from "../context/AuthContext";
 
+export const updatePushToken = async (pushToken) => {
+    try {
+        let userToken = await AsyncStorage.getItem('userToken')
+        let userInfo = await AsyncStorage.getItem('userInfo')
+        userInfo = JSON.parse(userInfo)
 
-export const updatePushToken = async(pushToken) => {
-    const { userToken } = useContext(AuthContext)
-    const { userInfo } = useContext(AuthContext)
-    //Get User Info
-    // let userToken = await AsyncStorage.getItem('userToken')
-    // axios.post(`${BASE_URL}/wp/v2/users/${userInfo.id}`, {
-    //     meta: {
-    //         pushToken: pushToken
-    //     },
-    //   },{
-    //     headers: {
-    //       'Authorization': 'Bearer ' + userToken
-    //     }
-    //   }).then(function (userResponse) {
-    //     console.log(userResponse.data);
-    //     // let _userResponse = userResponse.data
-
-    //     // setUserInfo(_userResponse)
-    //     // AsyncStorage.setItem('userInfo', JSON.stringify(_userResponse))
-    // }).catch(function (error) {
-    //     console.log('Error getting user info', error);
-    // });
-    console.log('Update');
+        if(userInfo) {
+            // Get User Info
+            axios.post(`${BASE_URL}/wp-json/wp/v2/users/${userInfo.id}`, {
+                meta: {
+                    push_token: pushToken
+                },
+              },{
+                headers: {
+                  'Authorization': 'Bearer ' + userToken
+                }
+              }).then(function (userResponse) {
+                // console.log(userResponse.data);
+                // let _userResponse = userResponse.data
+            }).catch(function (error) {
+                console.log('Error updatePushToken', error.response.data.message);
+            });
+        }
+    } catch (error) {
+        console.log(`updatePushToken error ${error}`);
+    }
 }
